c50bb940a38a110ff41344420a54069bfaff484f
mbarbine <barbine.michael@gmail.com>
2024-08-14 16:38:45 -0400

    fonts

--- plugins/pwa.js
+++ plugins/pwa.js
@@ -1,7 +1,7 @@
 import { defineNuxtPlugin } from '#app';
 
 export default defineNuxtPlugin(() => {
-  if ('serviceWorker' in navigator) {
+  if (import.meta.client && 'serviceWorker' in navigator) {
     navigator.serviceWorker.register('/sw.js').then((registration) => {
       console.log('Service Worker registered with scope:', registration.scope);
     }).catch((error) => {
